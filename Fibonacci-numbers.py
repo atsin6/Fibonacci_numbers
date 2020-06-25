5c85e747f51c765df0e28a32147966faf6d55b84
Atul Pal <66594774+atsin6@users.noreply.github.com>
2020-06-25 14:04:36 +0530

    Update Fibonacci-numbers.py

--- Fibonacci-numbers.py
+++ Fibonacci-numbers.py
@@ -1,7 +1,7 @@
 #fibonacci Numbers : 0,1,1,2,3,5,8,13,21,34,55,........
 x = 0
 y = 1
-a = int(input('Enter how many numbers you want :',))
+a = int(input('Enter how many fibonacci numbers you want to be printed:',))
 for i in range(a):
     print(x)
     print(y)
